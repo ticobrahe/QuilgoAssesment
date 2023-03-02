fd71875ccc5fd972793de28c71d50433418d6be1
ticobrahe <olabiyi.sam@gmail.com>
2023-03-02 15:59:18 +0100

    remove duplicate from array

---
+++ QuilgoAssesment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuilgoAssesment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static int[] RemoveDuplicate(int[] arr)
+        {
+            var dict = new Dictionary<int, int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (dict.ContainsKey(arr[i]))
+                    continue;
+
+                dict[arr[i]] = arr[i];
+            }
+
+            var length = dict.Keys.Count;
+            var result = new int[length];
+            int count = 0;
+
+            foreach (var item in dict.Keys)
+            {
+                result[count] = item;
+                count++;
+            }
+
+            return result;
+        }
+    }
+}
