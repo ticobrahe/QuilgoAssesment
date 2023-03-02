3e84529fa65338fbcc8d14614b704f331508bbb2
ticobrahe <olabiyi.sam@gmail.com>
2023-03-02 16:29:37 +0100

    highest number occurrence

--- QuilgoAssesment/Program.cs
+++ QuilgoAssesment/Program.cs
@@ -7,7 +7,7 @@ namespace QuilgoAssesment
     {
         static void Main(string[] args)
         {
-
+            
         }
 
         public static int[] RemoveDuplicate(int[] arr)
@@ -32,6 +32,36 @@ namespace QuilgoAssesment
             }
 
             return result;
+        }
+
+        public static int HigestOccurence(int[] arr)
+        {
+            if (arr.Length == 0)
+            {
+                return 0;
+            }
+            var dict = new Dictionary<int, int>();
+            int max = 0;
+            int occurence = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (dict.ContainsKey(arr[i]))
+                {
+                    dict[arr[i]] += 1;
+                    if (dict[arr[i]] > max)
+                    {
+                        max = dict[arr[i]];
+                        occurence = arr[i];
+                    }
+                }
+                else
+                {
+                    dict[arr[i]] = 1;
+                }
+                
+            }
+
+            return occurence;
         }
     }
 }
